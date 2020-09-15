3c2d71769a966e87ad7e150bc3201142750a7fa3
andrew Kotouch <Andrewkotouch@outlook.com>
2020-09-15 09:39:53 -0400

    Add project files.

---
+++ magicball/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace magicball
+{
+    class Program
+    {
+        static Random randomObject = new Random();
+
+
+        static void Main(string[] args)
+
+
+
+        {
+            QuestionLoop();
+        }
+        static string GetQuestionFromUser()
+        {
+            Console.Write("What question do you have?");
+            string questionString = Console.ReadLine();
+            return questionString;
+
+        }
+        static void QuestionLoop()
+        {
+            while (true)
+            {
+                string questionString = GetQuestionFromUser();
+                int randomNumber = randomObject.Next(4);
+                switch (randomNumber)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("no");
+                            break;
+
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("yes");
+                            break;
+
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine(" dont know why you asking me?");
+                            break;
+
+                        }
+                }
+
+            }
+
+        }
+    }
+}
+
